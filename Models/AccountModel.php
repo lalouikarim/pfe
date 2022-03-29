8bac734c4d6ceb282e14b8e06240fa696f739444
lalouikarim <lalouikarim@gmail.com>
2022-03-29 12:02:16 +0100

    added an account model

---
+++ Models/AccountModel.php
@@ -0,0 +1,20 @@
+<?php
+
+class AccountModel{
+    private $dbconnector;
+    private $dbconn;
+    public $auth;
+
+    public function __construct(){
+        // connect to db
+        require_once "../Config/DbConnect.php";
+        $this->dbconnector = new DbConnect();
+        $this->dbconn = $this->dbconnector->connect();
+
+        // require the authentication library
+        require '../Libraries/AuthLib/vendor/autoload.php';
+        $this->auth = new \Delight\Auth\Auth($this->dbconn);
+    }
+}
+
+?>
