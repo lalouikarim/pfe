4c32b09d9293e01e11c0cc661b1343fc56a0c8be
lalouikarim <lalouikarim@gmail.com>
2022-04-02 12:38:50 +0100

    added a method to set an account's role

--- Models/AccountModel.php
+++ Models/AccountModel.php
@@ -4,6 +4,7 @@ class AccountModel{
     private $dbconnector;
     protected $dbconn;
     public $auth;
+    public $role;
 
     public function __construct(){
         // connect to db
@@ -14,6 +15,27 @@ class AccountModel{
         // require the authentication library
         require '../Libraries/AuthLib/vendor/autoload.php';
         $this->auth = new \Delight\Auth\Auth($this->dbconn);
+    }
+
+    // set the role of a user
+    public function SetAccountRole($usernameOrEmail){
+        $stmt = $this->dbconn->prepare("SELECT id FROM teachers WHERE user_id = (SELECT id FROM users WHERE username = :username_or_email OR email = :username_or_email)");
+        $stmt->bindParam(":username_or_email", $usernameOrEmail);
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        if(empty($result)){
+            $stmt = $this->dbconn->prepare("SELECT id FROM admins WHERE user_id = (SELECT id FROM users WHERE username = :username_or_email OR email = :username_or_email)");
+            $stmt->bindParam(":username_or_email", $usernameOrEmail);
+            $stmt->execute();
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            if(empty($result)){
+                $this->role = "student";
+            } else{
+                $this->role = "admin";
+            }
+        } else{
+            $this->role = "teacher";
+        }
     }
 }
 
