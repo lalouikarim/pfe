ece52a61747c5e1975e2c1d7c56ef3895312dbe1
lalouikarim <lalouikarim@gmail.com>
2022-04-06 16:16:56 +0100

    added a method to retrieve the teacher details of an offer

--- Models/AdminModel.php
+++ Models/AdminModel.php
@@ -19,6 +19,15 @@ class AdminModel extends AccountModel{
         return $offersList;
     }
 
+    // retrieve an offer's teacher info
+    public function RetrieveOfferTeacherInfo($offerId){
+        $stmt = $this->dbconn->prepare("SELECT users.email, users.username, teachers.first_name, teachers.last_name, teachers.phone, teachers.card_photo, teachers.teacher_photo, teachers.cv_link FROM offers INNER JOIN teachers ON offers.teacher_id = teachers.id INNER JOIN users ON teachers.user_id = users.id WHERE offers.id = :offer_id");
+        $stmt->bindParam(":offer_id", $offerId);
+        $stmt->execute();
+        $teacherInfo = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $teacherInfo;
+    }
+
     // retreive the necessary offer details to send an email to the teacher
     public function RetrieveOfferForEmail($offerId){
         $stmt = $this->dbconn->prepare("SELECT users.email, offers.state, offers.commune, offers.level, offers.subject FROM offers INNER JOIN teachers ON offers.teacher_id = teachers.id INNER JOIN users ON teachers.user_id = users.id WHERE offers.id = :offer_id");
