c3e5a501dbf6d7f79f40917537f606b225953f87
lalouikarim <lalouikarim@gmail.com>
2022-04-21 16:16:26 +0100

    added a function that allows to rate an offer

--- Scripts/MainScripts.js
+++ Scripts/MainScripts.js
@@ -9,22 +9,22 @@ function LogOut(){
         }
     });
 }
-//script for btn products details 
+
+
 function showbtn(id) {
-// var mybtn = document.getElementById("btn");
     var mybtn1 ;
     mybtn1 = document.getElementById("show_teacher_details_popup_" + id );
-    mybtn1.style.transform = 'scale(1)'
-    //mybtn1 = document.getElementsByClassName("btn-product-details").id;
-    //mybtn.style.transform = 'scale(1)'
-    //mybtn1[0].style.transform = 'scale(1)'
+    mybtn1.style.transform = 'scale(1)';
+    var btn2 = document.getElementById("rate_offer_popup_" + id);
+    btn2.style.transform = 'scale(1)';
 }
 
 function hidebtn(id) {
 var mybtn1 ;
     mybtn1 = document.getElementById("show_teacher_details_popup_" + id );
     mybtn1.style.transform = 'scale(0)';
-
+    var btn2 = document.getElementById("rate_offer_popup_" + id);
+    btn2.style.transform = 'scale(0)';
 }
 
 function pagination(page, section){
@@ -94,5 +94,38 @@ function DisplayTeacherDetailsPopup(offerId){
                 $("#teacher_details_popup_" + offerId).html(data["teacher_details_html"]);
             }
         }
+    });
+}
+
+function RateOffers(){
+    $(".user-rating").each(function(index, element){
+        var id = this.id;
+        $("#" + id).barrating({
+            theme: 'fontawesome-stars', 
+            onSelect: function(value, text, event){
+                if (typeof(event) !== 'undefined'){
+                    var offerId = id.split('_')[3];
+                    var dataString = "&action=rateoffer&offer_id=" + offerId + "&offer_rating=" + value;
+                    $.ajax({
+                        url:"Controllers/UserController.php",
+                        type: 'post',
+                        data: dataString,
+                        success: function(data){
+                            data = JSON.parse(data);
+                            if(data["error"] != ""){
+                                $('#user_offer_rating_' + offerId).barrating('set', 0);
+                                alert(data["error"]);
+                            } else if(data["logged_in"] === false){
+                                $('#user_offer_rating_' + offerId).barrating('set', 0);
+                                window.open('Views/SignInView.html?req=rateoffer', '_blank', 'width=1000, height=400, top=200, left=300');
+                            } else{
+                                $("#offer_rating_" + offerId).barrating('set', Math.floor(data["avg_rating"]));
+                                $("#offer_rates_number_" + offerId).html(data["rates_number"]);
+                            }
+                        }
+                    });
+                }
+            }
+        });
     });
 }
